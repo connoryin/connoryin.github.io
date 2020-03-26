9bf70d039aead0acdaeb310bd6527000ed671376
connoryin <connor@sjtu.edu.cn>
2020-03-25 21:43:43 -0400

    most content finished

--- static/js/custom.js
+++ static/js/custom.js
@@ -104,7 +104,10 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01").animatedModal({modalTarget: 'securityModal'});
+		$("#demo02").animatedModal({modalTarget: 'privacyModal'});
+		$("#demo03").animatedModal({modalTarget: 'principalModal'});
+		$("#demo04").animatedModal({modalTarget: 'sohbingModal'});
 
     // Contact Form 	
 
